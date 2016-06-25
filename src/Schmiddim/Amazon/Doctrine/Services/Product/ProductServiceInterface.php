111d8d1fe770bf478033fd8cb681e84853a98f7e
ms <schmiddim@gmx.at>
2016-06-25 21:04:29 +0200

    annotations, new methods

--- src/Schmiddim/Amazon/Doctrine/Services/Product/ProductServiceInterface.php
+++ src/Schmiddim/Amazon/Doctrine/Services/Product/ProductServiceInterface.php
@@ -3,14 +3,28 @@
 
 namespace Schmiddim\Amazon\Doctrine\Services\Product;
 
-use Schmiddim\Amazon\Doctrine\Services\EntityServiceInterface;
-
-interface ProductServiceInterface extends EntityServiceInterface
+use Doctrine\ORM\EntityRepository;
+use Schmiddim\Amazon\Doctrine\Entities\Product;
+use Doctrine\ORM\EntityManager;
+interface ProductServiceInterface
 {
 
     public function getProductByAsin($asin);
 
     public function existsProductByAsin($asin);
 
+    /**
+     * @return EntityManager
+     */
+    public function getEntityManager();
+
+    /**
+     * @return  EntityRepository
+     */
+    public function getRepository();
+    /**
+     * @param \SimpleXMLElement $itemDetails
+     * @return Product
+     */
     public function createProductByXml(\SimpleXMLElement $itemDetails);
 }
