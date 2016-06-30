09dcc989d9a00a634e60991043e6dc298733b283
ms <schmiddim@gmx.at>
2016-06-30 15:45:56 +0200

    annotation

--- src/Schmiddim/Amazon/Doctrine/Services/ItemNotFound/ItemNotFoundServiceInterface.php
+++ src/Schmiddim/Amazon/Doctrine/Services/ItemNotFound/ItemNotFoundServiceInterface.php
@@ -4,6 +4,7 @@
 namespace Schmiddim\Amazon\Doctrine\Services\ItemNotFound;
 
 
+use Schmiddim\Amazon\Doctrine\Entities\ApaiIO\ItemNotFound;
 use Schmiddim\Amazon\Doctrine\Services\EntityServiceInterface;
 
 interface ItemNotFoundServiceInterface extends EntityServiceInterface
@@ -12,6 +13,10 @@ interface ItemNotFoundServiceInterface extends EntityServiceInterface
 
     public function findOneByAsin($asin);
 
+    /**
+     * @param array $asins
+     * @return ItemNotFound[]
+     */
     public function findByAsin($asins = array());
     public function existsByAsin($asin);
 }
